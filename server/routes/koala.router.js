6f87435e088efcf1de48dd0137c47440268ac420
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-09 16:22:19 -0500

    completed server side post

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -8,7 +8,28 @@ const koalaRouter = express.Router();
 
 
 // POST
+koalaRouter.post('/', (req,res) => {
+    const newKoalaTenent = req.body;
 
+    const queryText = `
+            INSERT INTO "koalas" ("name", "age", "gender", "transferReady", "notes")
+            VALUES ($1, $2, $3, $4, $5)
+        `;
+
+    pool.query( queryText ,[
+        newKoalaTenent.name,
+        newKoalaTenent.age,
+        newKoalaTenent.gender,
+        newKoalaTenent.transferReady,
+        newKoalaTenent.notes,
+
+    ]).then((results) => {
+        res.send(200);
+
+    }).catch((error) => {
+        res.send(500);
+    });
+});
 
 // PUT
 
