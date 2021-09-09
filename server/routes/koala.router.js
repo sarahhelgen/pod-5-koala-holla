6c7e7e1945bfd2ce51ee661846b3aa505f14cc2c
Sarah Helgen <sarah.helgen@gmail.com>
2021-09-09 15:43:11 -0500

    DB connected with PG!

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,8 +5,20 @@ const koalaRouter = express.Router();
 const pg = require('pg');
 
 const pool = new pg.Pool({
-    d
-})
+    database: 'koala_holla',
+    host: 'localhost',
+    port: '5432',
+    max: 10,
+    idleTimeoutMillis: 30000,
+});
+
+pool.on('connect', () =>{
+    console.log('PG connected to postgres!');
+});
+
+pool.on('error', (error) =>{
+    console.log('unable to connect to postgres!', error );
+});
 
 
 // GET
