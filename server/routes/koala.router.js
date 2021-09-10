62a30f9532e4da521d619f940b363f5df04c6db5
Tou Xiong <tlxiong.prime@outlook.com>
2021-09-10 09:20:35 -0500

    created PUT but still needs quertyText for db

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -11,6 +11,18 @@ const koalaRouter = express.Router();
 
 
 // PUT
+koalaRouter.put('/:id', (req,res) => {
+    const koalaId = req.params.id;
+
+    const queryText = '';
+
+    pool.query(queryText, [koalaId]).then((results) => {
+        res.send(200);
+        
+    }).catch((error) => {
+        res.send(500);
+    })
+})
 
 
 // DELETE
